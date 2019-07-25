ce9d96b9707ccf0c584ee9846c8ddccedbd425c6
stephenAJ <50453083+stephenAJ@users.noreply.github.com>
2019-07-25 14:37:32 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,9 @@
+import {Songs} from '../../app/songInfo';
+
+export const MYSONGS: Songs[]=[
+    {songName: "The good left undone", artist: "Rise Against", genre: "Melodic Hardcore", yearReleased: 2007, picture: './assets/images/riseAgainst.jpg'},
+    {songName: "Crawling", artist: "Linkin Park", genre: "Nu Metal", yearReleased: 2001, picture: './assets/images/hybridTheory.jpg'},
+    {songName: "Divinity", artist: "Memphis May fire", genre: "rock,Metalcore", yearReleased: 2014, picture: './assets/images/divinity.jpg'},
+    {songName: "There's No Going Back", artist: "Sick Puppies", genre: "Alternative Rock", yearReleased: 2013, picture: './assets/images/sickPuppies.png'}
+];
+
